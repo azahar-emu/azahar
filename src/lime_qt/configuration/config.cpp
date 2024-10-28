Lime3DS/Lime3DS

qt: Add diagonal directional input presets for circlepad and c-stick

src/lime_qt/configuration/config.cpp
@@ -32,20 +32,28 @@ const std::array<int, Settings::NativeButton::NumButtons> Config::default_button
     Qt::Key_O, Qt::Key_P, Qt::Key_1, Qt::Key_2, Qt::Key_B, Qt::Key_V,
 };
 
-const std::array<std::array<int, 5>, Settings::NativeAnalog::NumAnalogs> Config::default_analogs{{
+const std::array<std::array<int, 9>, Settings::NativeAnalog::NumAnalogs> Config::default_analogs{{
     {
-        Qt::Key_Up,
-        Qt::Key_Down,
-        Qt::Key_Left,
-        Qt::Key_Right,
-        Qt::Key_D,
+        Qt::Key_Up,         // Up
+        Qt::Key_Down,       // Down
+        Qt::Key_Left,       // Left
+        Qt::Key_Right,      // Right
+        Qt::Key_W,          // Up-Right
+        Qt::Key_Q,          // Up-Left
+        Qt::Key_E,          // Down-Right
+        Qt::Key_A,          // Down-Left
+        Qt::Key_D           // Modifier
     },
     {
-        Qt::Key_I,
-        Qt::Key_K,
-        Qt::Key_J,
-        Qt::Key_L,
-        Qt::Key_D,
+        Qt::Key_I,          // Up
+        Qt::Key_K,          // Down
+        Qt::Key_J,          // Left
+        Qt::Key_L,          // Right
+        Qt::Key_U,          // Up-Right
+        Qt::Key_H,          // Up-Left
+        Qt::Key_O,          // Down-Right
+        Qt::Key_B,          // Down-Left
+        Qt::Key_D           // Modifier
     },
 }};
 
@@ -382,10 +390,11 @@ void Config::ReadControlValues() {
             std::string default_param = InputCommon::GenerateAnalogParamFromKeys(
                 default_analogs[i][0], default_analogs[i][1], default_analogs[i][2],
                 default_analogs[i][3], default_analogs[i][4], 0.5f);
-            profile.analogs[i] = ReadSetting(QString::fromUtf8(Settings::NativeAnalog::mapping[i]),
-                                             QString::fromStdString(default_param))
-                                     .toString()
-                                     .toStdString();
+            profile.analogs[i] =
+                ReadSetting(QString::fromStdString(Settings::NativeAnalog::mapping[i]),
+                            QString::fromStdString(default_param))
+                    .toString()
+                    .toStdString();
             if (profile.analogs[i].empty())
                 profile.analogs[i] = default_param;
         }

src/lime_qt/configuration/configure_input.cpp
@@ -25,6 +25,10 @@ const std::array<std::string, ConfigureInput::ANALOG_SUB_BUTTONS_NUM>
         "down",
         "left",
         "right",
+        "up_left",
+        "up_right",
+        "down_left",
+        "down_right",
         "modifier",
     }};
 
@@ -33,6 +37,10 @@ enum class AnalogSubButtons {
     down,
     left,
     right,
+    up_left,
+    up_right,
+    down_left,
+    down_right,
     modifier,
 };
 
@@ -172,6 +180,10 @@ ConfigureInput::ConfigureInput(Core::System& _system, QWidget* parent)
             ui->buttonCircleDown,
             ui->buttonCircleLeft,
             ui->buttonCircleRight,
+            ui->buttonCircleUpLeft,
+            ui->buttonCircleUpRight,
+            ui->buttonCircleDownLeft,
+            ui->buttonCircleDownRight,
             nullptr,
         },
         {
@@ -179,6 +191,10 @@ ConfigureInput::ConfigureInput(Core::System& _system, QWidget* parent)
             ui->buttonCStickDown,
             ui->buttonCStickLeft,
             ui->buttonCStickRight,
+            ui->buttonCStickUpLeft,
+            ui->buttonCStickUpRight,
+            ui->buttonCStickDownLeft,
+            ui->buttonCStickDownRight,
             nullptr,
         },
     }};
@@ -468,12 +484,10 @@ void ConfigureInput::RestoreDefaults() {
     }
 
     for (int analog_id = 0; analog_id < Settings::NativeAnalog::NumAnalogs; analog_id++) {
-        for (int sub_button_id = 0; sub_button_id < ANALOG_SUB_BUTTONS_NUM; sub_button_id++) {
-            Common::ParamPackage params{InputCommon::GenerateKeyboardParam(
-                Config::default_analogs[analog_id][sub_button_id])};
-            SetAnalogButton(params, analogs_param[analog_id], analog_sub_buttons[sub_button_id]);
-        }
-        analogs_param[analog_id].Set("modifier_scale", 0.5f);
+        analogs_param[analog_id] = Common::ParamPackage{InputCommon::GenerateAnalogParamFromKeys(
+            Config::default_analogs[analog_id][0], Config::default_analogs[analog_id][1],
+            Config::default_analogs[analog_id][2], Config::default_analogs[analog_id][3],
+            Config::default_analogs[analog_id][4], 0.5f)};
     }
     UpdateButtonLabels();
 

azahar-emu/azahar

src/lime_qt/configuration/config.cpp
@@ -32,20 +32,28 @@ const std::array<int, Settings::NativeButton::NumButtons> QtConfig::default_butt
     Qt::Key_O, Qt::Key_P, Qt::Key_1, Qt::Key_2, Qt::Key_B, Qt::Key_V,
 };
 
-const std::array<std::array<int, 5>, Settings::NativeAnalog::NumAnalogs> QtConfig::default_analogs{{
+const std::array<std::array<int, 9>, Settings::NativeAnalog::NumAnalogs> QtConfig::default_analogs{{
     {
-        Qt::Key_Up,
-        Qt::Key_Down,
-        Qt::Key_Left,
-        Qt::Key_Right,
-        Qt::Key_D,
+        Qt::Key_Up,         // Up
+        Qt::Key_Down,       // Down
+        Qt::Key_Left,       // Left
+        Qt::Key_Right,      // Right
+        Qt::Key_W,          // Up-Right
+        Qt::Key_Q,          // Up-Left
+        Qt::Key_E,          // Down-Right
+        Qt::Key_A,          // Down-Left
+        Qt::Key_D           // Modifier
     },
     {
-        Qt::Key_I,
-        Qt::Key_K,
-        Qt::Key_J,
-        Qt::Key_L,
-        Qt::Key_D,
+        Qt::Key_I,          // Up
+        Qt::Key_K,          // Down
+        Qt::Key_J,          // Left
+        Qt::Key_L,          // Right
+        Qt::Key_U,          // Up-Right
+        Qt::Key_H,          // Up-Left
+        Qt::Key_O,          // Down-Right
+        Qt::Key_B,          // Down-Left
+        Qt::Key_D           // Modifier
     },
 }};
 
@@ -382,10 +390,11 @@ void QtConfig::ReadControlValues() {
             std::string default_param = InputCommon::GenerateAnalogParamFromKeys(
                 default_analogs[i][0], default_analogs[i][1], default_analogs[i][2],
                 default_analogs[i][3], default_analogs[i][4], 0.5f);
-            profile.analogs[i] = ReadSetting(QString::fromUtf8(Settings::NativeAnalog::mapping[i]),
-                                             QString::fromStdString(default_param))
-                                     .toString()
-                                     .toStdString();
+            profile.analogs[i] =
+                ReadSetting(QString::fromStdString(Settings::NativeAnalog::mapping[i]),
+                            QString::fromStdString(default_param))
+                    .toString()
+                    .toStdString();
             if (profile.analogs[i].empty())
                 profile.analogs[i] = default_param;
         }

src/lime_qt/configuration/configure_input.cpp
@@ -25,6 +25,10 @@ const std::array<std::string, ConfigureInput::ANALOG_SUB_BUTTONS_NUM>
         "down",
         "left",
         "right",
+        "up_left",
+        "up_right",
+        "down_left",
+        "down_right",
         "modifier",
     }};
 
@@ -33,6 +37,10 @@ enum class AnalogSubButtons {
     down,
     left,
     right,
+    up_left,
+    up_right,
+    down_left,
+    down_right,
     modifier,
 };
 
@@ -172,6 +180,10 @@ ConfigureInput::ConfigureInput(Core::System& _system, QWidget* parent)
             ui->buttonCircleDown,
             ui->buttonCircleLeft,
             ui->buttonCircleRight,
+            ui->buttonCircleUpLeft,
+            ui->buttonCircleUpRight,
+            ui->buttonCircleDownLeft,
+            ui->buttonCircleDownRight,
             nullptr,
         },
         {
@@ -179,6 +191,10 @@ ConfigureInput::ConfigureInput(Core::System& _system, QWidget* parent)
             ui->buttonCStickDown,
             ui->buttonCStickLeft,
             ui->buttonCStickRight,
+            ui->buttonCStickUpLeft,
+            ui->buttonCStickUpRight,
+            ui->buttonCStickDownLeft,
+            ui->buttonCStickDownRight,
             nullptr,
         },
     }};
@@ -469,12 +485,10 @@ void ConfigureInput::RestoreDefaults() {
     }
 
     for (int analog_id = 0; analog_id < Settings::NativeAnalog::NumAnalogs; analog_id++) {
-        for (int sub_button_id = 0; sub_button_id < ANALOG_SUB_BUTTONS_NUM; sub_button_id++) {
-            Common::ParamPackage params{InputCommon::GenerateKeyboardParam(
-                QtConfig::default_analogs[analog_id][sub_button_id])};
-            SetAnalogButton(params, analogs_param[analog_id], analog_sub_buttons[sub_button_id]);
-        }
-        analogs_param[analog_id].Set("modifier_scale", 0.5f);
+        analogs_param[analog_id] = Common::ParamPackage{InputCommon::GenerateAnalogParamFromKeys(
+            QtConfig::default_analogs[analog_id][0], QtConfig::default_analogs[analog_id][1],
+            QtConfig::default_analogs[analog_id][2], QtConfig::default_analogs[analog_id][3],
+            QtConfig::default_analogs[analog_id][4], 0.5f)};
     }
     UpdateButtonLabels();
 