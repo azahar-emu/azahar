c86e4a7347db20dc3f926a21dcb6269a687db2f5
Kleidis <167202775+kleidis@users.noreply.github.com>
2025-03-12 16:11:49 +0100

    Initial network room creation/joining support on Android
    Co-Authored-By: Gamer64 <76565986+Gamer64ytb@users.noreply.github.com>
    Co-Authored-By: zhang wei <zwdreams@gmail.com>

--- src/android/app/src/main/java/org/citra/citra_emu/ui/main/MainActivity.kt
+++ src/android/app/src/main/java/org/citra/citra_emu/ui/main/MainActivity.kt
@@ -38,6 +38,7 @@ import kotlinx.coroutines.launch
 import org.citra.citra_emu.R
 import org.citra.citra_emu.contracts.OpenFileResultContract
 import org.citra.citra_emu.databinding.ActivityMainBinding
+import org.citra.citra_emu.dialogs.NetPlayDialog
 import org.citra.citra_emu.features.settings.model.Settings
 import org.citra.citra_emu.features.settings.model.SettingsViewModel
 import org.citra.citra_emu.features.settings.ui.SettingsActivity
@@ -173,6 +174,11 @@ class MainActivity : AppCompatActivity(), ThemeProvider {
 
     override fun onDestroy() {
         super.onDestroy()
+    }
+
+    fun displayMultiplayerDialog() {
+        val dialog = NetPlayDialog(this)
+        dialog.show()
     }
 
     override fun setTheme(resId: Int) {
