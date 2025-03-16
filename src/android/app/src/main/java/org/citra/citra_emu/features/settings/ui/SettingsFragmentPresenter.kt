56af16d506a14b139c948b492d0cc8daf093b8c1
Kleidis <167202775+kleidis@users.noreply.github.com>
2025-03-16 18:42:26 +0100

    android: Add network configuration options for username, API URL, and token

--- src/android/app/src/main/java/org/citra/citra_emu/features/settings/ui/SettingsFragmentPresenter.kt
+++ src/android/app/src/main/java/org/citra/citra_emu/features/settings/ui/SettingsFragmentPresenter.kt
@@ -16,6 +16,7 @@ import androidx.preference.PreferenceManager
 import com.google.android.material.dialog.MaterialAlertDialogBuilder
 import kotlin.math.min
 import org.citra.citra_emu.CitraApplication
+import org.citra.citra_emu.NativeLibrary
 import org.citra.citra_emu.R
 import org.citra.citra_emu.display.PortraitScreenLayout
 import org.citra.citra_emu.display.ScreenLayout
@@ -98,6 +99,7 @@ class SettingsFragmentPresenter(private val fragmentView: SettingsFragmentView) 
             Settings.SECTION_CONTROLS -> addControlsSettings(sl)
             Settings.SECTION_RENDERER -> addGraphicsSettings(sl)
             Settings.SECTION_LAYOUT -> addLayoutSettings(sl)
+            Settings.SECTION_NETWORK -> addNetworkSettings(sl)
             Settings.SECTION_AUDIO -> addAudioSettings(sl)
             Settings.SECTION_DEBUG -> addDebugSettings(sl)
             Settings.SECTION_THEME -> addThemeSettings(sl)
@@ -179,6 +181,14 @@ class SettingsFragmentPresenter(private val fragmentView: SettingsFragmentView) 
                     0,
                     R.drawable.ic_fit_screen,
                     Settings.SECTION_LAYOUT
+                )
+            )
+            add(
+                SubmenuSetting(
+                    R.string.preferences_network,
+                    0,
+                    R.drawable.ic_network,
+                    Settings.SECTION_NETWORK
                 )
             )
             add(
@@ -1493,6 +1503,40 @@ class SettingsFragmentPresenter(private val fragmentView: SettingsFragmentView) 
             )
         }
 
+    }
+
+    private fun addNetworkSettings (sl: ArrayList<SettingsItem>) {
+        settingsActivity.setToolbarTitle(settingsActivity.getString(R.string.preferences_network))
+        sl.apply {
+            add(
+                StringInputSetting(
+                    StringSetting.NETWORK_USERNAME,
+                    R.string.network_username,
+                    R.string.network_username_description,
+                    StringSetting.NETWORK_USERNAME.key,
+                    StringSetting.NETWORK_USERNAME.defaultValue,
+                    10,
+                )
+            )
+            add(
+                StringInputSetting(
+                    StringSetting.WEB_API_URL,
+                    R.string.web_api_url,
+                    R.string.web_api_url_description,
+                    StringSetting.WEB_API_URL.key,
+                    StringSetting.WEB_API_URL.defaultValue,
+                )
+            )
+            add(
+                StringInputSetting(
+                    StringSetting.NETWORK_TOKEN,
+                    R.string.network_token,
+                    R.string.network_token_description,
+                    StringSetting.NETWORK_TOKEN.key,
+                    StringSetting.NETWORK_TOKEN.defaultValue,
+                )
+            )
+        }
     }
 
     private fun addAudioSettings(sl: ArrayList<SettingsItem>) {
