b4fdff0e03eff051acd721635cd479d1a1f28947
marsia <179024853+mars-ia@users.noreply.github.com>
2025-06-01 16:05:55 +0200

    android: Add `second_screen_opacity` option
    Co-Authored-By: Reg Tiangha <rtiangha@users.noreply.github.com>

--- src/android/app/src/main/java/org/citra/citra_emu/features/settings/model/FloatSetting.kt
+++ src/android/app/src/main/java/org/citra/citra_emu/features/settings/model/FloatSetting.kt
@@ -1,4 +1,4 @@
-// Copyright Citra Emulator Project / Lime3DS Emulator Project
+// Copyright Citra Emulator Project / Azahar Emulator Project
 // Licensed under GPLv2 or any later version
 // Refer to the license.txt file included.
 
@@ -10,6 +10,7 @@ enum class FloatSetting(
     override val defaultValue: Float
 ) : AbstractFloatSetting {
     LARGE_SCREEN_PROPORTION("large_screen_proportion",Settings.SECTION_LAYOUT,2.25f),
+    SECOND_SCREEN_OPACITY("custom_second_layer_opacity", Settings.SECTION_RENDERER, 100f),
     BG_BLUE("bg_blue", Settings.SECTION_RENDERER, 0.0f),
     BG_GREEN("bg_green", Settings.SECTION_RENDERER, 0.0f),
     BG_RED("bg_red", Settings.SECTION_RENDERER, 0.0f),
