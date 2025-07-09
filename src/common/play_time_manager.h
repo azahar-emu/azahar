a350c58e0f8244620d9a54dc572ca56afcfdb5d3
OpenSauce04 <opensauce04@gmail.com>
2025-07-09 22:27:05 +0100

    Updated license headers

--- src/common/play_time_manager.h
+++ src/common/play_time_manager.h
@@ -1,5 +1,6 @@
-// SPDX-FileCopyrightText: 2024 Citra Emulator Project
-// SPDX-License-Identifier: GPL-2.0-or-later
+// Copyright Citra Emulator Project / Azahar Emulator Project
+// Licensed under GPLv2 or any later version
+// Refer to the license.txt file included.
 
 #pragma once
 
