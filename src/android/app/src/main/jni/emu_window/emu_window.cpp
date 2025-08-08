8d53707755ace3a43a052708025d5a8ac31e819c
OpenSauce04 <opensauce04@gmail.com>
2025-08-08 19:27:27 +0100

    Removed unnecessary log message

--- src/android/app/src/main/jni/emu_window/emu_window.cpp
+++ src/android/app/src/main/jni/emu_window/emu_window.cpp
@@ -59,9 +59,6 @@ void EmuWindow_Android::OnFramebufferSizeChanged() {
 EmuWindow_Android::EmuWindow_Android(ANativeWindow* surface, bool is_secondary)
     : EmuWindow{is_secondary}, host_window(surface) {
     LOG_DEBUG(Frontend, "Initializing EmuWindow_Android");
-    if (is_secondary) {
-        LOG_DEBUG(Frontend, "Initializing EmuWindow_Android for secondary display");
-    }
     if (!surface) {
         LOG_CRITICAL(Frontend, "surface is nullptr");
         return;
