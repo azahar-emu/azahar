8f4a8bf24acb92f875068561ec31427fcca81a6e
yaasin-raki2 <yassin23raki@gmail.com>
2025-09-26 08:59:27 +0200

    added hotkeys to each screen layout and an extra one to toggle between two

--- src/citra_qt/citra_qt.h
+++ src/citra_qt/citra_qt.h
@@ -269,6 +269,8 @@ private slots:
     void AdjustSpeedLimit(bool increase);
     void UpdateSecondaryWindowVisibility();
     void ToggleScreenLayout();
+    void SetLayout(Settings::LayoutOption option);
+    void TogglePreviousLayout();
     void OnSwapScreens();
     void OnRotateScreens();
     void TriggerSwapScreens();
