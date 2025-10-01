3925ac81f5931a8608a15d67db827029f19157b5
David Griswold <novachild@gmail.com>
2025-10-01 22:15:58 +0300

    Add a new setting to specify which layouts to cycle

---
+++ src/citra_qt/configuration/configure_layout_cycle.cpp
@@ -0,0 +1,81 @@
+#include <QCloseEvent>
+#include <QDialog>
+#include "citra_qt/configuration/configure_layout_cycle.h"
+#include "ui_configure_layout_cycle.h"
+
+ConfigureLayoutCycle::ConfigureLayoutCycle(QWidget* parent)
+    : QDialog(parent), ui(std::make_unique<Ui::ConfigureLayoutCycle>()) {
+    ui->setupUi(this);
+    SetConfiguration();
+    ConnectEvents();
+}
+
+// You MUST define the destructor in the .cpp file
+ConfigureLayoutCycle::~ConfigureLayoutCycle() = default;
+
+void ConfigureLayoutCycle::ConnectEvents() {
+    connect(ui->buttonBox, &QDialogButtonBox::accepted, this,
+            &ConfigureLayoutCycle::ApplyConfiguration);
+    connect(ui->globalCheck, &QCheckBox::stateChanged, this, &ConfigureLayoutCycle::UpdateGlobal);
+}
+
+void ConfigureLayoutCycle::SetConfiguration() {
+    if (Settings::IsConfiguringGlobal()) {
+        ui->globalCheck->setChecked(true);
+        ui->globalCheck->setVisible(false);
+    } else {
+        ui->globalCheck->setChecked(Settings::values.layouts_to_cycle.UsingGlobal());
+        ui->checkGroup->setDisabled(Settings::values.layouts_to_cycle.UsingGlobal());
+    }
+    for (auto option : Settings::values.layouts_to_cycle.GetValue()) {
+        switch (option) {
+        case Settings::LayoutOption::Default:
+            ui->defaultCheck->setChecked(true);
+            break;
+        case Settings::LayoutOption::SingleScreen:
+            ui->singleCheck->setChecked(true);
+            break;
+        case Settings::LayoutOption::LargeScreen:
+            ui->largeCheck->setChecked(true);
+            break;
+        case Settings::LayoutOption::SideScreen:
+            ui->sidebysideCheck->setChecked(true);
+            break;
+        case Settings::LayoutOption::SeparateWindows:
+            ui->separateCheck->setChecked(true);
+            break;
+        case Settings::LayoutOption::HybridScreen:
+            ui->hybridCheck->setChecked(true);
+            break;
+        case Settings::LayoutOption::CustomLayout:
+            ui->customCheck->setChecked(true);
+            break;
+        }
+    }
+}
+
+void ConfigureLayoutCycle::ApplyConfiguration() {
+    std::vector<Settings::LayoutOption> newSetting{};
+    if (ui->defaultCheck->isChecked())
+        newSetting.push_back(Settings::LayoutOption::Default);
+    if (ui->singleCheck->isChecked())
+        newSetting.push_back(Settings::LayoutOption::SingleScreen);
+    if (ui->sidebysideCheck->isChecked())
+        newSetting.push_back(Settings::LayoutOption::SideScreen);
+    if (ui->largeCheck->isChecked())
+        newSetting.push_back(Settings::LayoutOption::LargeScreen);
+    if (ui->separateCheck->isChecked())
+        newSetting.push_back(Settings::LayoutOption::SeparateWindows);
+    if (ui->hybridCheck->isChecked())
+        newSetting.push_back(Settings::LayoutOption::HybridScreen);
+    if (ui->customCheck->isChecked())
+        newSetting.push_back(Settings::LayoutOption::CustomLayout);
+    Settings::values.layouts_to_cycle = newSetting;
+    accept();
+}
+
+void ConfigureLayoutCycle::UpdateGlobal() {
+    Settings::values.layouts_to_cycle.SetGlobal(ui->globalCheck->isChecked());
+    ui->checkGroup->setDisabled(ui->globalCheck->isChecked());
+    ui->checkGroup->repaint(); // Force visual update
+}
---
+++ src/citra_qt/configuration/configure_layout_cycle.h
@@ -0,0 +1,32 @@
+// Copyright 2018 Citra Emulator Project
+// Licensed under GPLv2 or any later version
+// Refer to the license.txt file included.
+#pragma once
+
+#include <memory>
+#include <QDialog>
+#include "common/settings.h"
+
+namespace Ui {
+class ConfigureLayoutCycle;
+}
+
+class ConfigureLayoutCycle : public QDialog {
+    Q_OBJECT
+
+public:
+    explicit ConfigureLayoutCycle(QWidget* parent = nullptr);
+    ~ConfigureLayoutCycle() override;
+
+public slots:
+    void ApplyConfiguration();
+
+private slots:
+
+private:
+    void SetConfiguration();
+    void ConnectEvents();
+    void UpdateGlobal();
+
+    std::unique_ptr<Ui::ConfigureLayoutCycle> ui;
+};
