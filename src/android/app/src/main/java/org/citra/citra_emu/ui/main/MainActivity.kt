f911af41974ab4062cac0e3987e196d50fbb234e
OpenSauce04 <opensauce04@gmail.com>
2025-12-24 19:02:06 +0000

    MainActivity.kt: Added missing import

--- src/android/app/src/main/java/org/citra/citra_emu/ui/main/MainActivity.kt
+++ src/android/app/src/main/java/org/citra/citra_emu/ui/main/MainActivity.kt
@@ -8,6 +8,7 @@ import android.Manifest
 import android.content.Intent
 import android.content.pm.PackageManager
 import android.net.Uri
+import android.os.Build
 import android.os.Bundle
 import android.os.Environment
 import android.view.View
