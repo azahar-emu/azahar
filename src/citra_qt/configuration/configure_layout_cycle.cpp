7a5457df302e7478262fd5863696d40972402238
David Griswold <novachild@gmail.com>
2026-02-26 19:10:03 +0300

    fix license headers

--- src/citra_qt/configuration/configure_layout_cycle.cpp
+++ src/citra_qt/configuration/configure_layout_cycle.cpp
@@ -1,3 +1,6 @@
+// Copyright Citra Emulator Project / Azahar Emulator Project
+// Licensed under GPLv2 or any later version
+// Refer to the license.txt file included.
 #include <QCloseEvent>
 #include <QDialog>
 #include <QMessageBox>
