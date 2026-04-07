ea1eea9061030a22ff0f585d54ae2766ab52b691
jbm11208 <81182113+jbm11208@users.noreply.github.com>
2026-04-07 15:41:25 -0400

    Move location of where is_powered_on variable is updated to fix Discord Rich Presence not updating correctly

--- src/core/core.cpp
+++ src/core/core.cpp
@@ -511,6 +511,8 @@ System::ResultStatus System::Init(Frontend::EmuWindow& emu_window,
                                   Kernel::MemoryMode memory_mode, u32 num_cores) {
     LOG_DEBUG(HW_Memory, "initialized OK");
 
+    is_powered_on = true;
+
     memory = std::make_unique<Memory::MemorySystem>(*this);
 
     timing = std::make_unique<Timing>(num_cores, Settings::values.cpu_clock_percentage.GetValue(),
@@ -591,8 +593,6 @@ System::ResultStatus System::Init(Frontend::EmuWindow& emu_window,
     }
 
     LOG_DEBUG(Core, "Initialized OK");
-
-    is_powered_on = true;
 
     return ResultStatus::Success;
 }
@@ -687,9 +687,6 @@ void System::RegisterImageInterface(std::shared_ptr<Frontend::ImageInterface> im
 
 void System::Shutdown(bool is_deserializing) {
 
-    // Shutdown emulation session
-    is_powered_on = false;
-
     gpu.reset();
     if (!is_deserializing) {
         lle_modules.clear();
@@ -721,6 +718,9 @@ void System::Shutdown(bool is_deserializing) {
     memory.reset();
 
     LOG_DEBUG(Core, "Shutdown OK");
+
+    // Shutdown emulation session
+    is_powered_on = false;
 }
 
 void System::Reset() {
