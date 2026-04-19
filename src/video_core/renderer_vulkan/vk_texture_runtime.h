a979260601fee79742555b5504ba5dece6e9d9a1
Wunkolo <Wunkolo@gmail.com>
2026-04-19 10:49:09 -0700

    renderer_vulkan: Fix `Framebuffer::sample_count` move-operator
    `sample_count` needs to be move/copied over.
    
    Also reorder the accessor order to match the declaration of variables.

--- src/video_core/renderer_vulkan/vk_texture_runtime.h
+++ src/video_core/renderer_vulkan/vk_texture_runtime.h
@@ -309,7 +309,8 @@ public:
           formats(std::exchange(
               other.formats, {VideoCore::PixelFormat::Invalid, VideoCore::PixelFormat::Invalid})),
           width(std::exchange(other.width, 0)), height(std::exchange(other.height, 0)),
-          res_scale(std::exchange(other.res_scale, 1)) {}
+          res_scale(std::exchange(other.res_scale, 1)),
+          sample_count(std::exchange(other.sample_count, 1)) {}
 
     Framebuffer& operator=(Framebuffer&& other) noexcept {
         VideoCore::FramebufferParams::operator=(std::move(other));
@@ -324,6 +325,7 @@ public:
         width = std::exchange(other.width, 0);
         height = std::exchange(other.height, 0);
         res_scale = std::exchange(other.res_scale, 1);
+        sample_count = std::exchange(other.sample_count, 1);
 
         return *this;
     }
@@ -352,12 +354,12 @@ public:
         return render_pass;
     }
 
-    u8 Samples() const noexcept {
-        return sample_count;
-    }
-
     u32 Scale() const noexcept {
         return res_scale;
+    }
+
+    u8 Samples() const noexcept {
+        return sample_count;
     }
 
 private:
