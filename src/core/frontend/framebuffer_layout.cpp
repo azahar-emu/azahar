264e3cd68c2b16b1fcf297365fa68e27ef1db2b2
OpenSauce04 <opensauce04@gmail.com>
2026-04-26 18:25:18 +0100

    Rename SecondaryDisplayLayout::ReversePrimary for consistency

--- src/core/frontend/framebuffer_layout.cpp
+++ src/core/frontend/framebuffer_layout.cpp
@@ -324,8 +324,8 @@ FramebufferLayout AndroidSecondaryLayout(u32 width, u32 height) {
         return HybridScreenLayout(width, height, false, Settings::values.upright_screen.GetValue());
     case Settings::SecondaryDisplayLayout::None:
         // this should never happen - if "none" is set this method shouldn't run - but if it does,
-        // somehow, use ReversePrimary
-    case Settings::SecondaryDisplayLayout::ReversePrimary:
+        // somehow, use OppositeScreenOnly
+    case Settings::SecondaryDisplayLayout::OppositeScreenOnly:
     default:
         return SingleFrameLayout(width, height, !Settings::values.swap_screen.GetValue(),
                                  Settings::values.upright_screen.GetValue());
