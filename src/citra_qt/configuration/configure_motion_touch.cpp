f499155ba212b0d766bd6681f08a3c566e6aa12b
RedBlackAka <140876408+RedBlackAka@users.noreply.github.com>
2026-05-16 21:31:50 +0200

    Link to Cemuhook on Cemu's wiki

--- src/citra_qt/configuration/configure_motion_touch.cpp
+++ src/citra_qt/configuration/configure_motion_touch.cpp
@@ -100,7 +100,7 @@ ConfigureMotionTouch::ConfigureMotionTouch(QWidget* parent)
     ui->udp_learn_more->setOpenExternalLinks(true);
     ui->udp_learn_more->setText(
         tr("<a "
-           "href='https://cemuhook.sshnuke.net/padudpserver.html'><span "
+           "href='https://wiki.cemu.info/wiki/Cemuhook'><span "
            "style=\"text-decoration: underline; color:#039be5;\">Learn More</span></a>"));
 
     timeout_timer->setSingleShot(true);
