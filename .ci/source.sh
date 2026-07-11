e1861aac0b62f20f858fabc0475a4f670fbbd34f
goodusername123 <61405538+goodusername123@users.noreply.github.com>
2026-07-11 13:59:50 -0500

    Update source.sh with fallback and other test
    makes it so lziprecover failing doesn't error out the script and also tests if it would even be uploaded in the first place.

--- .ci/source.sh
+++ .ci/source.sh
@@ -20,6 +20,11 @@ git archive-all --include "${COMPAT_LIST}" --include GIT-COMMIT --include GIT-TA
 
 cd artifacts/
 tarlz -v -9z "${REV_NAME}.tar"
-lziprecover -v -Fc "${REV_NAME}.tar.lz"
+lziprecover -v -Fc "${REV_NAME}.tar.lz" || fec_failed=1
+if [ ${fec_failed:-0} -eq 1 ]
+then
+ echo "fec file creation failed! this is likely due to lziprecover being out of date."
+fi
 sha256sum "${REV_NAME}.tar.lz" > "${REV_NAME}.tar.lz.sha256sum"
+touch "${REV_NAME}.tar.lz.fec" # THIS IS A TEST MAKE SURE TO REMOVE IT!!
 cd ..
